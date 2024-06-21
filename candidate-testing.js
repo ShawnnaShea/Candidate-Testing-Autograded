ab74e9aa2f2236b62bec905208d68b55ba85c722
Shawnna <lee.sshea4658@gmail.com>
2024-06-20 22:05:05 -0500

    Part One candidate testing completed

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
@@ -18,12 +18,13 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  var candidateName = prompt('Hello, what is your name?');
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  var question = prompt('Hello, what is your name?');
+  console.log('Hello!' + question + 'Here is a short quiz for you.');
 
 }
 
@@ -37,12 +38,12 @@ function gradeQuiz(candidateAnswers) {
 
 
   return grade;
-}
+};
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log('Hello!' + candidateName + 'Here is a short quiz for you.');
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
