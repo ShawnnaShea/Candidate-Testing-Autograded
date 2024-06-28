c848e5e1f5a75b0e7160abf96fb7b8fcee39a222
Shawnna <lee.sshea4658@gmail.com>
2024-06-27 23:22:17 -0500

    candidate testing part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,30 +11,57 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+"Who was the first American woman in space? ",
+"True or false: 5 kilometer == 5000 meters? ",
+"(5 + 3)/2 * 10 = ? ",
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+"What is the minimum crew size for the ISS? " 
+];
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3",
+];
+let candidateAnswers = [""];
+let score = Number("");
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  var candidateName = prompt('Hello, what is your name?');
+  let candidateName = input.question('Please enter your name: ');
+  console.log("");
+  return candidateName;
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  var question = prompt('Hello, what is your name?');
-  console.log('Hello!' + question + 'Here is a short quiz for you.');
-
+  for (i = 0; i < questions.length; i++) {
+    candidateAnswers[i] = input.question(`${i+1} ${questions[i]}`);
+    console.log(`Your Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}`);
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+      score += 1;
+    }
+    console.log("");
+  }       
+return candidateAnswers;
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+let scorePer = (score / (questions.length)) * 100;
+if (scorePer >= 80) {
+  grade = "PASSED";
+  console.log(`>>> Overall Grade: ${scorePer} % ${score} of ${questions.length} responses correct <<<\n>>> Status: ${grade} <<<`);
+} else {
+  grade = "FAILED";
+  console.log(`>>> Overall Grade: ${scorePer} % ${score} of ${questions.length} responses correct <<<\n>>> Status: ${grade} <<<`);
+}
 
-
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+ //TODO 3.2 use this variable to calculate the candidates score.
 
 
   return grade;
